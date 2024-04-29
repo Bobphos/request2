332802f71b00546bb2be8db5f537c2314408dba8
Bobphos <zegameplay52@gmail.com>
2024-04-29 15:56:37 -0300

    bob

--- main.go
+++ main.go
@@ -4,14 +4,21 @@ import (
     "fmt"
     "net/http"
 )
-func main (){
-    resp, err := http.Get("https://distopia.savi2w.workers.dev/")
-    if err != nil {
-		fmt.Println("", err)
-		return
-	}
-    defer resp.Body.Close()
 
-    contentType := resp.Header.Get("Distopia")
-    fmt.Println("Distopia", contentType)
+func main() {
+    client := http.Client{
+        CheckRedirect: func(req *http.Request, via []*http.Request) error {
+            return http.ErrUseLastResponse
+        },
+    }
+
+    resp, err := client.Get("https://distopia.savi2w.workers.dev/")
+
+    if err != nil {
+        fmt.Println(err.Error())
+        return
+    }
+    fmt.Println("Status Code: ", resp.Status)
+    resp.Body.Close()
+    fmt.Println("Body response: ", resp.Header.Get("Distopia"))
 }
